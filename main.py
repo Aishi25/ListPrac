6df120d0f98a5e185425679a9f0cb5fb4a4e1837
Aishi25 <aishiagarwal11@gmail.com>
2020-08-20 21:48:31 +0000

    List Prac

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 hobbies = ["Playing soccer", "Reading a book", "Annoying sibling", "Listening to music", "Working with technology"]
-print("My hobbies \n")
+print("My hobbies")
 
 hobbies.append("Drawing")
 hobbies.append("Coloring")
@@ -7,15 +7,43 @@ hobbies.append("Biking")
 for i in range(len(hobbies)):
   print(hobbies[i])
 
-print("\n")
+
 print("\n")
 
 import random
 number = []
 sum = 0
-for i in range(5):
+for i in range(3):
   number.append(random.randint(1,100))
   sum = sum + number[i]
 print(sum)
 avg = sum/len(number)
 print("Average is " + str(avg))
+
+print("\n")
+
+import random
+number = []
+lar = 0
+for i in range(3):
+  number.append(random.randint(1,10))
+  print(number[i])
+
+  if (number[i] > lar):
+    lar = number[i]
+print("Largest number:" +str(lar))
+
+
+print("\n")
+
+name = []
+while True:
+  n = input("Enter a name. Enter stop to end this. ")
+  name.append(n)
+  if (n == "stop" or "Stop"):
+    break;
+for i in range(len(name)-1):
+  print(name[i])
+
+print("\n")
+
